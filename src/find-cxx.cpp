2bc014dbbff46e40fcd78f2eb6c8cfbb24af0b1e
Brendan <brewinn@protonmail.com>
2023-04-22 15:01:29 -0400

    Add functionality to find-cxx
    Find by type and find by name now work, with find by name utilizing
    regular expressions.

--- src/find-cxx.cpp
+++ src/find-cxx.cpp
@@ -1,10 +1,53 @@
 #include "find-cxx.hpp"
+#include <filesystem>
 #include <iostream>
+#include <regex>
+
+namespace fs = std::filesystem;
 
 void find_by_name(const char *path, const char *name) {
-  std::cout << "Find by name " << name << "on path " << path << "\n";
+  try {
+    std::regex re(name);
+    for (const auto &entry : fs::recursive_directory_iterator(path)) {
+      if (std::regex_search(entry.path().c_str(), re)) {
+        std::cout << entry.path().c_str() << "\n";
+      }
+    }
+  } catch (const std::regex_error &e) {
+    std::cerr << "Error in forming regular expression: " << e.what() << '\n';
+    std::exit(4);
+  }
 }
 
 void find_by_type(const char *path, const char type) {
-  std::cout << "Find by type " << type << " on path " << path << "\n";
+  // Check for valid type
+  // Maybe an enum or variant would also work, but I like the simplicity of just
+  // using char
+  switch (type) {
+  case 'l': {
+    for (auto entry : fs::recursive_directory_iterator(path)) {
+      if (fs::is_symlink(entry)) {
+        std::cout << entry.path().c_str() << '\n';
+      }
+    }
+    break;
+  }
+  case 'f':
+    for (auto entry : fs::recursive_directory_iterator(path)) {
+      if (fs::is_regular_file(entry)) {
+        std::cout << entry.path().c_str() << '\n';
+      }
+    }
+    break;
+  case 'd':
+    for (auto entry : fs::recursive_directory_iterator(path)) {
+      if (fs::is_directory(entry)) {
+        std::cout << entry.path().c_str() << '\n';
+      }
+    }
+    break;
+  default:
+    std::cerr << "Invalid type\n";
+    std::exit(3);
+  }
 }
